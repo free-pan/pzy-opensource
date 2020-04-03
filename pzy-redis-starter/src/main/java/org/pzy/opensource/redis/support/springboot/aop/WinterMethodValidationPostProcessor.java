7fb906d416c3cee88be67c6dcd1d26d47aa86548
free pan <sd4015700@126.com>
2020-04-03 21:55:51 +0800

    docs: 完善注释

--- pzy-redis-starter/src/main/java/org/pzy/opensource/redis/support/springboot/aop/WinterMethodValidationPostProcessor.java
+++ pzy-redis-starter/src/main/java/org/pzy/opensource/redis/support/springboot/aop/WinterMethodValidationPostProcessor.java
@@ -10,6 +10,7 @@ import org.springframework.core.Ordered;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 /**
+ * 自定义`MethodValidationPostProcessor`类, 覆盖`postProcessAfterInitialization`解决`MethodValidationInterceptor`执行优先级问题
  * @author pan
  * @date 2020/3/30
  */
@@ -23,19 +24,24 @@ public class WinterMethodValidationPostProcessor extends MethodValidationPostPro
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) {
+        // 如果是AOP相关的基础组件bean，如ProxyProcessorSupport类及其子类，则直接返回
         if (this.advisor == null || bean instanceof AopInfrastructureBean) {
             // Ignore AOP infrastructure such as scoped proxies.
             return bean;
         }
 
         if (bean instanceof Advised) {
+            // 如果已经是Advised的，即已经是被动态代理的实例，则直接添加advisor
             Advised advised = (Advised) bean;
             if (!advised.isFrozen() && isEligible(AopUtils.getTargetClass(bean))) {
+                // 如果没有被frozen(即冷冻，不再做改动的动态代理实例)且是Eligbile(合适的)，则把其添加到advisor中。根据配置决定插入位置
                 // Add our local Advisor to the existing proxy's Advisor chain...
                 if (this.beforeExistingAdvisors) {
                     advised.addAdvisor(0, this.advisor);
                 } else {
+                    // 获取已有切面
                     Advisor[] advisorArr = advised.getAdvisors();
+                    // 遍历已有切面与当前切面的order值比较, 找到第一个比当前切面order值大的切面, 并记下该位置
                     Integer curAdvisorPos = null;
                     for (int i = 0; i < advisorArr.length; i++) {
                         Advisor tmp = advisorArr[i];
@@ -51,6 +57,7 @@ public class WinterMethodValidationPostProcessor extends MethodValidationPostPro
                     if (null == curAdvisorPos) {
                         advised.addAdvisor(this.advisor);
                     } else {
+                        // 在第一个比当前切面order值大位置插入当前切面, 其它切面一次往后移一位
                         advised.addAdvisor(curAdvisorPos, this.advisor);
                     }
                 }
@@ -59,6 +66,7 @@ public class WinterMethodValidationPostProcessor extends MethodValidationPostPro
         }
 
         if (isEligible(bean, beanName)) {
+            // 如果是Eligible合适的，且还不是被代理的类，则创建一个代理类的实例并返回
             ProxyFactory proxyFactory = prepareProxyFactory(bean, beanName);
             if (!proxyFactory.isProxyTargetClass()) {
                 evaluateProxyInterfaces(bean.getClass(), proxyFactory);
