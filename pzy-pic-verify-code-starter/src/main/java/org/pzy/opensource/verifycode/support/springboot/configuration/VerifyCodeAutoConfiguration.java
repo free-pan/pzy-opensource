adb3f0702bce50eb6cb0b9ae564a9a9949b74082
free pan <sd4015700@126.com>
2020-04-12 17:16:38 +0800

    docs: 完善文档

--- pzy-pic-verify-code-starter/src/main/java/org/pzy/opensource/verifycode/support/springboot/configuration/VerifyCodeAutoConfiguration.java
+++ pzy-pic-verify-code-starter/src/main/java/org/pzy/opensource/verifycode/support/springboot/configuration/VerifyCodeAutoConfiguration.java
@@ -3,6 +3,7 @@ package org.pzy.opensource.verifycode.support.springboot.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.pzy.opensource.comm.util.JsonUtil;
 import org.pzy.opensource.verifycode.controller.VerificationCodeController;
+import org.pzy.opensource.verifycode.domain.constant.VerificationCodeConstant;
 import org.pzy.opensource.verifycode.support.springboot.filter.VerificationCodeFilter;
 import org.pzy.opensource.verifycode.support.springboot.properties.VerifyCodeConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,10 +32,10 @@ public class VerifyCodeAutoConfiguration {
     public void init(){
         if (log.isDebugEnabled()) {
             log.debug("图片验证码以及过滤器启用!");
-            log.debug("验证码的有效期为:[{}]秒, 会对这些地址执行验证码校验:[{}], 验证码验证不通过时会转入:[{}]", verifyCodeConfigProperties.getExpiresSeconds(), JsonUtil.toJsonString(verifyCodeConfigProperties.getFilterUrls()), verifyCodeConfigProperties.getErrorRedirectUrl());
+            log.debug("验证码的有效期为:[{}]秒, 会对这些地址执行验证码校验:{}, 验证码验证不通过时会转入:[{}]", verifyCodeConfigProperties.getExpiresSeconds(), JsonUtil.toJsonString(verifyCodeConfigProperties.getFilterUrls()), verifyCodeConfigProperties.getErrorRedirectUrl());
             log.debug("1. 生成图片验证码之前需要先通过VerificationCodeController类下的接口获取客户端id.");
             log.debug("2. 需要进行图片验证码验证的请求地址需要配置到[component.pic-verify-code.filterUrls]中.这样VerificationCodeFilter才会对该请求进行验证验证.");
-            log.debug("3. 无论生成验证码图片还是进行验证码验证都需要携带第一步中获取到客户端id.");
+            log.debug("3. 无论生成验证码图片还是进行验证码验证都需要携带第一步中获取到客户端id.客户端id的参数名为:[{}]", VerificationCodeConstant.CLIENT_ID);
             log.debug("4. 请使用 VerificationCodeUtil.loadCheckCodeVerifyStatus(), VerificationCodeUtil.loadVerifyCodeValidateFailType() 帮助类中的方法获取验证码过滤器的实时执行状态, 以及验证码过滤器执行结果!");
         }
     }
